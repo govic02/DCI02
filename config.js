6387d37d4a9695dd508f45d2a673ffeed492ddfd
nicolas gonzalez <nicolas.gonzalez@govic.cl>
2024-11-11 10:13:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // config.js convertido a ESM
 
 const credentials = {
-    client_id: 'pxK3vawavXwIA0r6cxA5mIlgXTco2kjIhgybbyAgmHkgWtzM',
-    client_secret: 'rbnPs5vBrqVm80xiAVAo5qlr4uXnVY6SJaDoVeyJIlWdeBk9dsu8QuTqrBAEGqWE',
+    client_id: 'CLIENT_ID',
+    client_secret: 'CLIENT_SECRET',
     callback_url: process.env.FORGE_CALLBACK_URL
 };
+
 
 const scopes = {
     internal: ['bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write'],
@@ -16,13 +17,3 @@ export default {
     scopes
 };
 
-//
-//pxK3vawavXwIA0r6cxA5mIlgXTco2kjIhgybbyAgmHkgWtzM
-//rbnPs5vBrqVm80xiAVAo5qlr4uXnVY6SJaDoVeyJIlWdeBk9dsu8QuTqrBAEGqWE
-
-
-/*
- client_id: '49FkrIoZFbuRA64ORi63YpE6djBWJz8M',
-    client_secret: 'kypZruU2hH3TKscA',
-    callback_url: process.env.FORGE_CALLBACK_URL
-*/
